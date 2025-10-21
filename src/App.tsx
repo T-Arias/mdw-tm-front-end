1b6e1ecae2dbc5638a8716e38581ee242c5cc424
tomas arias <tomas.arias2001@gmail.com>
2025-10-21 00:59:15 -0300

    add routing and user management components

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,18 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import type { User } from './types/user';
-import { PlayerListComponent } from './components/PlayerListComponent';
-import { fetchUser } from './services/userService';
-import { Card } from './components/Card';
+import { Link } from 'react-router';
+import { AppRouter } from './Router/AppRouter';
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [user, setUser] = useState('');
-  const [users, setUsers] = useState<User[]>([]);
-
-  const handlerOnClick = (countParam:number) => {
-    setCount(countParam + 1);
-    setUser('Tomas');
-  }
-
-  useEffect(() => {
-    console.log('componente montado');
-    async function fetchUserData() {
-      const userJson = await fetchUser();
-      setUsers(userJson);
-    }
-    fetchUserData();
-  }, [])
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        {user &&
-          <h1>Bienvenido usuario {user}</h1>
-        }
+      <nav>
+        <Link to={'/home'}>Home</Link>
+        <Link to={'/admin'}>Admin</Link>
+        <Link to={'/analytics'}>analytics</Link>
+        <Link to={'/home'}>Home</Link>
+      </nav>
+      <AppRouter />
 
-        {/* <ol>
-          {users.map((user: User) => (
-            <li key={user.id}>{user.name} - {user.email}</li>
-          ))}
-        </ol> */}
-        {/* <PlayerListComponent users={users}/> */}
-        <Card user={users[0]} present={true} />
-
-
-
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => handlerOnClick(count)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
